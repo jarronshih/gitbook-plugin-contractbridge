11c4d7765a72d378106c0b87319518275a434d84
Jarron Shih (RD-US) <jarronsh@US-JARRONS-MAC.local>
2015-10-17 23:12:51 -0700

    Add bidding table

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-
 module.exports = {
+    book: {
+        assets: './book',
+        css: ['biddingsystemTable.css']
+    },
+
     blocks: {
-        // Author will be able to write "{% myTag %}World{% endMyTag %}"
-        myTag: {
+        biddingSystemTable: {
+            blocks: ['seq', 'bids'],
             process: function(blk) {
-                return "Hello "+blk.body;
+                var bs_table = {
+                    seq: "",
+                    bids: []
+                };
+                blk.blocks.forEach(function(item) {
+                    if (item.body && item.name == 'seq') {
+                        bs_table.seq = item.body;
+                    } else if (item.body && item.name == 'bids') {
+                        item.body.split('\n').forEach(function(it) {
+                            if (it) {
+                                ary = it.split("##");
+                                bs_table.bids.push({auction: ary[0], auctiondef: ary[1]});
+                            }
+                        });
+                    }
+                });
+                return '<div class="biddingsystemTable">'
+                        + '<div class="seq">' + bs_table.seq + '</div>'
+                        + '<div class="bids">' + bs_table.bids.map(function (bid) {
+                            return '<div class="bid"><div class="auction">' + bid.auction + '</div>'
+                                + '<div class="auctiondef">' + bid.auctiondef + '</div></div>';
+                        }).reduce(function (x,y) { return x+y; }) + '</div>'
+                    + '</div>';
             }
+        },
+
+        handBox:{
+        },
+
+        auctionBox:{
+        },
+
+        quizzBox:{
         }
     },
-};
 
+    // filters: {},
+    // hooks: {}
+};
