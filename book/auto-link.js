783c4997817bec43ef2b525338a7a0c3091502a5
Jarron Shih <jarronshih@gmail.com>
2015-10-30 17:42:21 -0700

    dump to 0.2.2, enhance auto link performance

--- book/auto-link.js
+++ book/auto-link.js
@@ -1,12 +1,16 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
-        $('.summary [data-path] a[href]')
+        $('ul.summary [data-path] a[href]')
             .each(function(){
                 title = $(this).html().replace(/<([^ >]+)[^>]*>.*?<\/\1>|<[^\/]+\/>/ig, "").trim();
                 link = $(this).attr('href');
 
-                $( ".bids , p:contains("+title+")" ).each(function( index ) {
-                    $(this).html($(this).html().replace(title, '<a href=\"'+link+'\">'+title+'</a>'));
+                $( "section .bids-biddef , section p:contains("+title+")" ).each(function( index ) {
+                    text = $(this).html();
+                    if (text.indexOf(title) != -1) {
+                        re = new RegExp(title, 'gi');
+                        $(this).html(text.replace(re, '<a href=\"'+link+'\">'+title+'</a>'));
+                    }
                 });
         });
     });
